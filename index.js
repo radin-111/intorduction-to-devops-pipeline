7aad88d9198d47e4305547de27101650617789a4
radin-111 <radinsheikh87@gmail.com>
2025-09-12 19:55:08 +0600

    adding express

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-console.log("Hi CI/CD Pipeline");
-console.log("Running index.js");
+const express = require("express");
+
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("My server is running");
+});
+
+app.listen(port, () => {
+  console.log(`My server is running on port ${port}`);
+});
